71c5e4ce45730900ff1817ba5a2151dcc84f86f7
Estifanos58 <estifkebe08@gmail.com>
2025-05-13 22:38:49 +0300

    update: navbar complite

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import logoImage from '@/assets/images/logo.svg'
+import Button from "@/components/Button";
+import { a } from "framer-motion/client";
 
 const navLinks = [
     { label: "Home", href: "#" },
@@ -9,13 +11,20 @@ const navLinks = [
 ];
 
 export default function Navbar() {
-    return <section className="py-4">
-        <div className="container">
-            <div className="grid grid-cols-2 border border-white/15 rounded-full p-2 px-4 items-center">
+    return <section className="py-4 lg:py-8">
+        <div className="container max-w-5xl">
+            <div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full p-2 px-4  md:pr-2 items-center">
                 <div>
-                    <Image src={logoImage} alt="Layers logo" className="h-9 w-auto "/>
+                    <Image src={logoImage} alt="Layers logo" className="h-9 md:h-auto w-auto "/>
                 </div>
-                <div className="flex justify-end">
+                <div className=" hidden lg:flex justify-center items-center">
+                   <nav className="flex gap-6 font-medium">
+                    {navLinks.map(link=>(
+                        <a href={link.href} key={link.label}>{link.label}</a>
+                    ))}
+                    </nav> 
+                </div>
+                <div className="flex  justify-end gap-4">
                     <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width={24}
@@ -26,12 +35,14 @@ export default function Navbar() {
                     strokeWidth="2"
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    className="feather feather-menu mr-2"
+                    className="feather feather-menu mr-2 md:hidden"
                     >
                         <line x1="3" y1={12} x2={21} y2={12}></line>
                         <line x1="3" y1={6} x2={21} y2={6}></line>
                         <line x1="3" y1={18} x2={21} y2={18}></line>
                     </svg>
+                    <Button variant="secondary">Log In</Button>
+                    <Button variant="primary">Sign Up</Button>
                 </div>
             </div>
         </div>
