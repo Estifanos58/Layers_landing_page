94381fe882076fb50d50b7e3e3d8c810cb9441b6
Estifanos58 <estifkebe08@gmail.com>
2025-05-14 16:16:27 +0300

    update: hero section complete

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,24 +1,28 @@
-import React, { HTMLAttributes } from "react";
+import React, { ButtonHTMLAttributes} from "react";
 import { cva } from "class-variance-authority";
 
-const classes = cva("border hidden  md:block h-12 rounded-full px-6 font-medium", {
+const classes = cva("border hidden  md:inline-flex items-center h-12 rounded-full px-6 font-medium", {
     variants: {
         variant: {
             primary: "bg-lime-400 text-neutral-950 border-lime-400",
             secondary: "border-white text-white bg-transparent",
         },
+        size: {
+            sm: "h-10",
+        }
     },
 });
 
 const Button = (
     props: {
         variant: "primary" | "secondary";
-    } & HTMLAttributes<HTMLButtonElement>
+        size?: "sm";
+    } & ButtonHTMLAttributes<HTMLButtonElement>
 ) => {
-    const { variant, className, ...otherProps } = props;
+    const { variant,size, className, ...otherProps } = props;
     return (
         <button
-            className={classes({ variant: variant, className: className })}
+            className={classes({ variant, size, className })}
             {...otherProps}
         />
     );
