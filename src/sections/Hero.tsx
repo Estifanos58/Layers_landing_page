6c02075eadd73f54695661aa7bce9b62dc82495f
Estifanos58 <estifkebe08@gmail.com>
2025-05-15 22:09:42 +0300

    update: hero animation added

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,26 +1,107 @@
+"use client";
+
 import Button from "@/components/Button";
 import designExampleImage from "@/assets/images/design-example-1.png";
 import designeExampleImage2 from "@/assets/images/design-example-2.png";
 import Image from "next/image";
 import Pointer from "@/components/Pointer";
+import { animate, useAnimate, motion } from "framer-motion";
+import { useEffect } from "react";
+import cursorYouImage from '@/assets/images/cursor-you.svg'
 
 export default function Hero() {
+    const [leftDesignScope, leftDesignAnimate] = useAnimate();
+    const [leftPointerScope, leftPointerAnimate] = useAnimate();
+    const [rightDesignScope, rightDesignAnimate] = useAnimate();
+    const [rightPointerScope, rightPointerAnimate] = useAnimate();
+
+    useEffect(() => {
+        leftDesignAnimate([
+            [leftDesignScope.current, { opacity: [1] }, { duration: 0.5 }],
+            [leftDesignScope.current, { y: 0, x: 0 }, { duration: 0.5 }],
+        ]);
+
+        leftPointerAnimate([
+            [leftPointerScope.current, { opacity: [1] }, { duration: 0.5 }],
+            [leftPointerScope.current, { y: 0, x: -100 }, { duration: 0.5 }],
+            [
+                leftPointerScope.current,
+                { x: 0, y: [0, 16, 0] },
+                { duration: 0.5, ease: "easeInOut" },
+            ],
+        ]);
+
+        rightDesignAnimate([
+            [
+                rightDesignScope.current,
+                { opacity: [1] },
+                { duration: 0.5, delay: 1.5 },
+            ],
+            [rightDesignScope.current, { x: 0, y: 0 }, { duration: 0.5 }],
+        ]);
+
+        rightPointerAnimate([
+            [
+                rightPointerScope.current,
+                { opacity: 1 },
+                { duration: 0.5, delay: 1.5 },
+            ],
+            [rightPointerScope.current, { x: 175, y: 0 }, { duration: 0.5 }],
+            [
+                rightPointerScope.current,
+                { x: 0, y: [0, 20, 0] },
+                { duration: 0.5 },
+            ],
+        ]);
+    }, []);
+
     return (
         <>
-            <section className="py-24 overflow-x-clip">
+            <section className="py-24 overflow-x-clip" style={{
+                cursor: `url(${cursorYouImage.src}), auto`,
+            }}>
                 <div className="container relative">
-                    <div className="hidden lg:block absolute -left-32 top-16">
-                        <Image src={designExampleImage} alt="design example 1"/>
-                    </div>
-                    <div className="hidden lg:block absolute -right-64 -top-16">
-                        <Image src={designeExampleImage2} alt="design example 2"/>
-                    </div>
-                    <div className="hidden lg:block absolute left-56 top-96">
-                        <Pointer name="Andrea"/>
-                    </div>
-                    <div className="hidden lg:block absolute right-80 -top-4">
-                        <Pointer name="Bryan" color="red"/>
-                    </div>
+                    <motion.div
+                        ref={leftDesignScope}
+                        initial={{ opacity: 0, y: 100, x: -100 }}
+                        drag
+                        className="hidden lg:block absolute -left-32 top-16"
+                    >
+                        <Image
+                            src={designExampleImage}
+                            draggable="false"
+                            alt="design example 1"
+                        />
+                    </motion.div>
+
+                    <motion.div
+                        ref={leftPointerScope}
+                        initial={{ opacity: 0, y: 100, x: -200 }}
+                        className="hidden lg:block absolute left-56 top-96"
+                    >
+                        <Pointer name="Andrea" />
+                    </motion.div>
+
+                    <motion.div
+                        ref={rightDesignScope}
+                        initial={{ opacity: 0, x: 100, y: 100 }}
+                        drag
+                        className="hidden lg:block absolute -right-64 -top-16"
+                    >
+                        <Image
+                            src={designeExampleImage2}
+                            draggable="false"
+                            alt="design example 2"
+                        />
+                    </motion.div>
+
+                    <motion.div
+                        ref={rightPointerScope}
+                        initial={{ opacity: 0, x: 260, y: 100 }}
+                        className="hidden lg:block absolute right-80 -top-4"
+                    >
+                        <Pointer name="Bryan" color="red" />
+                    </motion.div>
                     <div className="flex justify-center">
                         <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
                             ✨ $7.5M seed round raised
@@ -41,7 +122,14 @@ export default function Hero() {
                             placeholder="Enter your email"
                             className="bg-transparent px-4 md:flex-1 w-full"
                         />
-                        <Button type="submit" variant="primary" className="whitespace-nowrap" size="sm">Sign Up</Button>
+                        <Button
+                            type="submit"
+                            variant="primary"
+                            className="whitespace-nowrap"
+                            size="sm"
+                        >
+                            Sign Up
+                        </Button>
                     </form>
                 </div>
             </section>
