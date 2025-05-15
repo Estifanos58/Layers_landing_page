0ded4065e321a0489c71e1bcd14347ac12670519
Estifanos58 <estifkebe08@gmail.com>
2025-05-15 21:32:01 +0300

    update: navbar animation done

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 import React, { ButtonHTMLAttributes} from "react";
 import { cva } from "class-variance-authority";
 
-const classes = cva("border hidden  md:inline-flex items-center h-12 rounded-full px-6 font-medium", {
+const classes = cva("border md:inline-flex items-center h-12 rounded-full px-6 font-medium", {
     variants: {
         variant: {
             primary: "bg-lime-400 text-neutral-950 border-lime-400",
